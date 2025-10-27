3146096f4a5f83af4dceee3bd45babf8463ff566
Hbelguith <161472004+Hbelguith@users.noreply.github.com>
2025-10-27 08:49:15 +0100

    Microsoft_drive

--- backend/open_webui/functions.py
+++ backend/open_webui/functions.py
@@ -79,7 +79,7 @@ def get_function_module_by_id(request: Request, pipe_id: str):
 
 
 async def get_function_models(request):
-    pipes = Functions.get_functions_by_type("pipe", active_only=True)
+    pipes = Functions.get_functions_by_type("pipe", active_only=True) 
     pipe_models = [] 
 
     for pipe in pipes:
