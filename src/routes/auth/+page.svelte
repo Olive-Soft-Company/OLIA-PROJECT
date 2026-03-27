e7737a91a3d7fa065da39f4528ef79bc6c3c5212
Skandar Allouche <skandar.allouche@olivesoft.fr>
2026-03-27 16:49:03 +0100

    minor change

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -191,7 +191,7 @@
 			if (!onboarding && !error && !form) {
 				const providers = Object.keys($config?.oauth?.providers ?? {});
 				if (providers.length > 0) {
-					window.location.href = `${WEBUI_BASE_URL}/oauth/microsoft/login`;
+					window.location.href = `${WEBUI_BASE_URL}/oauth/${providers[0]}/login`;
 					return;
 				}
 			}
